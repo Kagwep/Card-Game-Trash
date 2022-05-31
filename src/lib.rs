555339acefa8c415a0327bd109ef6ee3a3d29d94
kagwe <kagwepeter07@gmail.com>
2022-06-01 00:15:46 +0300

    Fc

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,95 @@
+use std::vec;
+
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::near_bindgen;
-
+#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(Debug,PartialEq)]
+enum status_state{
+    InProgress,
+    UnderReview,
+    Complete,
+}
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    // SETUP CONTRACT STATE
+#[derive(Debug)]
+pub struct Project{
+    project_id:u8,
+    project_name:String,
+    start_date:String,
+    status:status_state,
+    state_percentage:String,
+    members:Vec<String>,
+    no_of_tasks:Vec<String>,
+}
+#[near_bindgen]
+#[derive(Debug)]
+pub struct Task{
+    task_id:u8,
+    task_name:String,
+    start_date:String,
+    status:status_state,
+    state_percentage:String,
+}
+#[near_bindgen]
+#[derive(Debug)]
+pub struct User{
+    user_id:u8,
+    user_name:String,
+    user_rank:String,
+    task_assigned:String,
+
 }
 
 #[near_bindgen]
-impl Contract {
-    // ADD CONTRACT METHODS HERE
+impl  Task {
+
+    fn list_add(&self){
+
+        let mut task_in_progress:Vec<u8> = Vec::new();
+        let mut task_under_review:Vec<u8> = Vec::new();
+        let mut task_completed: Vec<u8> = Vec::new();
+       
+
+    
+        
+         if self.status == status_state::InProgress {
+            task_in_progress.push(self.task_id);
+            if  task_under_review.iter().any(|&i| i==self.task_id){
+                task_under_review.retain(|&x| x != self.task_id);
+            }
+            if task_completed.iter().any(|&i| i==self.task_id){
+                task_completed.retain(|&x| x != self.task_id);
+            }
+
+  
+         }
+         else if self.status == status_state::UnderReview {
+
+            task_under_review.push(self.task_id);
+            if   task_in_progress.iter().any(|&i| i==self.task_id){
+                task_in_progress.retain(|&x| x != self.task_id);
+            }
+            if task_completed.iter().any(|&i| i==self.task_id){
+                task_completed.retain(|&x| x != self.task_id);
+            }
+   
+         }
+         else{
+            task_completed.push(self.task_id);
+            if   task_under_review.iter().any(|&i| i==self.task_id){
+                task_under_review.retain(|&x| x != self.task_id);
+            }
+            if task_in_progress.iter().any(|&i| i==self.task_id){
+                task_in_progress.retain(|&x| x != self.task_id);
+            }
+            
+
+         }
+
+    }
+
+
 }
+
 
 /*
  * the rest of this file sets up unit tests
@@ -33,6 +112,10 @@ mod tests {
         builder.predecessor_account_id(predecessor);
         builder
     }
+
+    #[test]
+    fn list_add(){
+        }
 
     // TESTS HERE
 }
